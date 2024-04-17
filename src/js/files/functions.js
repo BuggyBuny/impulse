bd441587e74c87d7b1ce5b4e026812cb66218814
BuggyBuny <buggybuny@gmail.com>
2024-04-17 13:31:40 +0500

    first commit

---
+++ src/js/files/functions.js
@@ -0,0 +1,395 @@
+/* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
+export function isWebp() {
+	// Проверка поддержки webp
+	function testWebP(callback) {
+		let webP = new Image();
+		webP.onload = webP.onerror = function () {
+			callback(webP.height == 2);
+		};
+		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	}
+	// Добавление класса _webp или _no-webp для HTML
+	testWebP(function (support) {
+		let className = support === true ? 'webp' : 'no-webp';
+		document.documentElement.classList.add(className);
+	});
+}
+/* Проверка мобильного браузера */
+export let isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+/* Добавление класса touch для HTML если браузер мобильный */
+export function addTouchClass() {
+	// Добавление класса _touch для HTML если браузер мобильный
+	if (isMobile.any()) document.documentElement.classList.add('touch');
+}
+// Добавление loaded для HTML после полной загрузки страницы
+export function addLoadedClass() {
+	window.addEventListener("load", function () {
+		setTimeout(function () {
+			document.documentElement.classList.add('loaded');
+		}, 0);
+	});
+}
+// Получение хеша в адресе сайта
+export function getHash() {
+	if (location.hash) { return location.hash.replace('#', ''); }
+}
+// Указание хеша в адресе сайта
+export function setHash(hash) {
+	history.pushState('', '', hash);
+}
+// Учет плавающей панели на мобильных устройствах при 100vh
+export function fullVHfix() {
+	const fullScreens = document.querySelectorAll('[data-fullscreen]');
+	if (fullScreens.length && isMobile.any()) {
+		window.addEventListener('resize', fixHeight);
+		function fixHeight() {
+			let vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty('--vh', `${vh}px`);
+		}
+		fixHeight();
+	}
+}
+// Вспомогательные модули плавного расскрытия и закрытия объекта ======================================================================================================================================================================
+export let _slideUp = (target, duration = 500, showmore = 0) => {
+	if (!target.classList.contains('_slide')) {
+		target.classList.add('_slide');
+		target.style.transitionProperty = 'height, margin, padding';
+		target.style.transitionDuration = duration + 'ms';
+		target.style.height = `${target.offsetHeight}px`;
+		target.offsetHeight;
+		target.style.overflow = 'hidden';
+		target.style.height = showmore ? `${showmore}px` : `0px`;
+		target.style.paddingTop = 0;
+		target.style.paddingBottom = 0;
+		target.style.marginTop = 0;
+		target.style.marginBottom = 0;
+		window.setTimeout(() => {
+			target.hidden = !showmore ? true : false;
+			!showmore ? target.style.removeProperty('height') : null;
+			target.style.removeProperty('padding-top');
+			target.style.removeProperty('padding-bottom');
+			target.style.removeProperty('margin-top');
+			target.style.removeProperty('margin-bottom');
+			!showmore ? target.style.removeProperty('overflow') : null;
+			target.style.removeProperty('transition-duration');
+			target.style.removeProperty('transition-property');
+			target.classList.remove('_slide');
+		}, duration);
+	}
+}
+export let _slideDown = (target, duration = 500, showmore = 0) => {
+	if (!target.classList.contains('_slide')) {
+		target.classList.add('_slide');
+		target.hidden = target.hidden ? false : null;
+		showmore ? target.style.removeProperty('height') : null;
+		let height = target.offsetHeight;
+		target.style.overflow = 'hidden';
+		target.style.height = showmore ? `${showmore}px` : `0px`;
+		target.style.paddingTop = 0;
+		target.style.paddingBottom = 0;
+		target.style.marginTop = 0;
+		target.style.marginBottom = 0;
+		target.offsetHeight;
+		target.style.transitionProperty = "height, margin, padding";
+		target.style.transitionDuration = duration + 'ms';
+		target.style.height = height + 'px';
+		target.style.removeProperty('padding-top');
+		target.style.removeProperty('padding-bottom');
+		target.style.removeProperty('margin-top');
+		target.style.removeProperty('margin-bottom');
+		window.setTimeout(() => {
+			target.style.removeProperty('height');
+			target.style.removeProperty('overflow');
+			target.style.removeProperty('transition-duration');
+			target.style.removeProperty('transition-property');
+			target.classList.remove('_slide');
+		}, duration);
+	}
+}
+export let _slideToggle = (target, duration = 500) => {
+	if (target.hidden) {
+		return _slideDown(target, duration);
+	} else {
+		return _slideUp(target, duration);
+	}
+}
+// Вспомогательные модули блокировки прокрутки и скочка ====================================================================================================================================================================================================================================================================================
+export let bodyLockStatus = true;
+export let bodyLockToggle = (delay = 500) => {
+	if (document.documentElement.classList.contains('lock')) {
+		bodyUnlock(delay);
+	} else {
+		bodyLock(delay);
+	}
+}
+export let bodyUnlock = (delay = 500) => {
+	let body = document.querySelector("body");
+	if (bodyLockStatus) {
+		let lock_padding = document.querySelectorAll("[data-lp]");
+		setTimeout(() => {
+			for (let index = 0; index < lock_padding.length; index++) {
+				const el = lock_padding[index];
+				el.style.paddingRight = '0px';
+			}
+			body.style.paddingRight = '0px';
+			document.documentElement.classList.remove("lock");
+		}, delay);
+		bodyLockStatus = false;
+		setTimeout(function () {
+			bodyLockStatus = true;
+		}, delay);
+	}
+}
+export let bodyLock = (delay = 500) => {
+	let body = document.querySelector("body");
+	if (bodyLockStatus) {
+		let lock_padding = document.querySelectorAll("[data-lp]");
+		for (let index = 0; index < lock_padding.length; index++) {
+			const el = lock_padding[index];
+			el.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+		}
+		body.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+		document.documentElement.classList.add("lock");
+
+		bodyLockStatus = false;
+		setTimeout(function () {
+			bodyLockStatus = true;
+		}, delay);
+	}
+}
+
+// Модуь работы с табами =======================================================================================================================================================================================================================
+export function tabs() {
+	const tabs = document.querySelectorAll('[data-tabs]');
+	let tabsActiveHash = [];
+
+	if (tabs.length > 0) {
+		const hash = location.hash.replace('#', '');
+		if (hash.startsWith('tab-')) {
+			tabsActiveHash = hash.replace('tab-', '').split('-');
+		}
+		tabs.forEach((tabsBlock, index) => {
+			tabsBlock.classList.add('_tab-init');
+			tabsBlock.setAttribute('data-tabs-index', index);
+			tabsBlock.addEventListener("click", setTabsAction);
+			initTabs(tabsBlock);
+		});
+
+		// Получение табов с медиа запросами
+		const tabsMedia = Array.from(tabs).filter(function (item, index, self) {
+			return item.dataset.tabs;
+		});
+		// Инициализация табов с медиа запросами
+		if (tabsMedia.length > 0) {
+			initMediaTabs(tabsMedia);
+		}
+	}
+	// Инициализация табов с медиа запросами
+	function initMediaTabs(tabsMedia) {
+		const breakpointsArray = [];
+		tabsMedia.forEach(item => {
+			const breakpointValue = item.dataset.tabs;
+
+			const tabsBreakpointsObject = {};
+			tabsBreakpointsObject.value = breakpointValue;
+			tabsBreakpointsObject.item = item;
+
+			breakpointsArray.push(tabsBreakpointsObject);
+		});
+
+		// Получаем уникальные брейкпоинты
+		let mediaQueries = breakpointsArray.map(function (item) {
+			return `(max-width:${item.value}px),${item.value}`;
+		});
+		mediaQueries = mediaQueries.filter(function (item, index, self) {
+			return self.indexOf(item) === index;
+		});
+
+		// Работаем с каждым брейкпоинтом
+		mediaQueries.forEach(breakpoint => {
+			const paramsArray = breakpoint.split(",");
+			const matchMedia = window.matchMedia(paramsArray[0]);
+			const mediaBreakpoint = paramsArray[1];
+
+			// Объекты с нужными условиями
+			const tabsMediaArray = breakpointsArray.filter(function (item) {
+				if (item.value === mediaBreakpoint) {
+					return true;
+				}
+			});
+
+			// Событие
+			matchMedia.addEventListener("change", function () {
+				setTitlePosition(tabsMediaArray, matchMedia);
+			});
+			setTitlePosition(tabsMediaArray, matchMedia);
+		});
+	}
+	// Установка позиций заголовков
+	function setTitlePosition(tabsMediaArray, matchMedia) {
+		tabsMediaArray.forEach(tabsMediaItem => {
+			tabsMediaItem = tabsMediaItem.item;
+			const tabsTitles = tabsMediaItem.querySelector('[data-tabs-titles]');
+			const tabsTitleItems = tabsMediaItem.querySelectorAll('[data-tabs-title]');
+			const tabsContent = tabsMediaItem.querySelector('[data-tabs-body]');
+			const tabsContentItems = tabsMediaItem.querySelectorAll('[data-tabs-item]');
+			tabsContentItems.forEach((tabsContentItem, index) => {
+				if (matchMedia.matches) {
+					tabsContent.append(tabsTitleItems[index]);
+					tabsContent.append(tabsContentItem);
+					tabsMediaItem.classList.add('_tab-spoller');
+				} else {
+					tabsTitles.append(tabsTitleItems[index]);
+					tabsMediaItem.classList.remove('_tab-spoller');
+				}
+			});
+		});
+	}
+	// Работа с контентом
+	function initTabs(tabsBlock) {
+		const tabsTitles = tabsBlock.querySelectorAll('[data-tabs-titles]>*');
+		const tabsContent = tabsBlock.querySelectorAll('[data-tabs-body]>*');
+		const tabsBlockIndex = tabsBlock.dataset.tabsIndex;
+		const tabsActiveHashBlock = tabsActiveHash[0] == tabsBlockIndex;
+
+		if (tabsActiveHashBlock) {
+			const tabsActiveTitle = tabsBlock.querySelector('[data-tabs-titles]>._tab-active');
+			tabsActiveTitle.classList.remove('_tab-active');
+		}
+		if (tabsContent.length > 0) {
+			tabsContent.forEach((tabsContentItem, index) => {
+				tabsTitles[index].setAttribute('data-tabs-title', '');
+				tabsContentItem.setAttribute('data-tabs-item', '');
+
+				if (tabsActiveHashBlock && index == tabsActiveHash[1]) {
+					tabsTitles[index].classList.add('_tab-active');
+				}
+				tabsContentItem.hidden = !tabsTitles[index].classList.contains('_tab-active');
+			});
+		}
+	}
+	function setTabsStatus(tabsBlock) {
+		const tabsTitles = tabsBlock.querySelectorAll('[data-tabs-title]');
+		const tabsContent = tabsBlock.querySelectorAll('[data-tabs-item]');
+		const tabsBlockIndex = tabsBlock.dataset.tabsIndex;
+
+		function isTabsAnamate(tabsBlock) {
+			if (tabsBlock.hasAttribute('data-tabs-animate')) {
+				return tabsBlock.dataset.tabsAnimate > 0 ? tabsBlock.dataset.tabsAnimate : 500;
+			}
+		}
+		const tabsBlockAnimate = isTabsAnamate(tabsBlock);
+
+		if (tabsContent.length > 0) {
+			tabsContent.forEach((tabsContentItem, index) => {
+				if (tabsTitles[index].classList.contains('_tab-active')) {
+					if (tabsBlockAnimate) {
+						_slideDown(tabsContentItem, tabsBlockAnimate);
+					} else {
+						tabsContentItem.hidden = false;
+					}
+					location.hash = `tab-${tabsBlockIndex}-${index}`;
+				} else {
+					if (tabsBlockAnimate) {
+						_slideUp(tabsContentItem, tabsBlockAnimate);
+					} else {
+						tabsContentItem.hidden = true;
+					}
+				}
+			});
+		}
+	}
+	function setTabsAction(e) {
+		const el = e.target;
+		if (el.closest('[data-tabs-title]')) {
+			const tabTitle = el.closest('[data-tabs-title]');
+			const tabsBlock = tabTitle.closest('[data-tabs]');
+			if (!tabTitle.classList.contains('_tab-active') && !tabsBlock.querySelectorAll('._slide').length) {
+
+				const tabActiveTitle = tabsBlock.querySelector('[data-tabs-title]._tab-active');
+				if (tabActiveTitle) {
+					tabActiveTitle.classList.remove('_tab-active');
+				}
+
+				tabTitle.classList.add('_tab-active');
+				setTabsStatus(tabsBlock);
+			}
+			e.preventDefault();
+		}
+	}
+}
+// Модуь работы с меню (бургер) =======================================================================================================================================================================================================================
+export function menuInit() {
+	if (document.querySelector(".icon-menu")) {
+		document.addEventListener("click", function (e) {
+			if (bodyLockStatus && e.target.closest('.icon-menu')) {
+				bodyLockToggle();
+				document.documentElement.classList.toggle("menu-open");
+			}
+		});
+	};
+}
+export function menuOpen() {
+	bodyLock();
+	document.documentElement.classList.add("menu-open");
+}
+export function menuClose() {
+	bodyUnlock();
+	document.documentElement.classList.remove("menu-open");
+}
+// Модуль "галерея" =======================================================================================================================================================================================================================
+$(function () {
+	$('.minimized').click(function (event) {
+		var i_path = $(this).attr('src');
+		$('body').append('<div id="overlay"></div><div id="magnify"><img src="' + i_path + '"><div id="close-popup"><i></i></div></div>');
+		$('#magnify').css({
+			left: ($(document).width() - $('#magnify').outerWidth()) / 2,
+			top: ($(window).height() - $('#magnify').outerHeight()) / 2
+		});
+		$('#overlay, #magnify').fadeIn('fast');
+	});
+
+	$('body').on('click', '#close-popup, #overlay', function (event) {
+		event.preventDefault();
+
+		$('#overlay, #magnify').fadeOut('fast', function () {
+			$('#close-popup, #magnify, #overlay').remove();
+		});
+	});
+});
+
+// Модуль попапов ===========================================================================================================================================================================================================================
+import { Popup } from "../libs/popup.js";
+export const initPopups = (logging = false, init = true) => new Popup({ logging: logging, init: init });
+
+//================================================================================================================================================================================================================================================================================================================
+// Прочие полезные функции ================================================================================================================================================================================================================================================================================================================
+//================================================================================================================================================================================================================================================================================================================
+
+// Получить цифры из строки
+export function getDigFromString(item) {
+	return parseInt(item.replace(/[^\d]/g, ''))
+}
+// Форматирование цифр типа 100 000 000
+export function getDigFormat(item) {
+	return item.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ");
+}
+// Убрать класс из всех элементов массива
+export function removeClasses(array, className) {
+	for (var i = 0; i < array.length; i++) {
+		array[i].classList.remove(className);
+	}
+}
+// Уникализация массива
+export function uniqArray(array) {
+	return array.filter(function (item, index, self) {
+		return self.indexOf(item) === index;
+	});
+}
+// Функция получения индекса внутри родителя
+export function indexInParent(parent, element) {
+	const array = Array.prototype.slice.call(parent.children);
+	return Array.prototype.indexOf.call(array, element);
+};
+
+//================================================================================================================================================================================================================================================================================================================
